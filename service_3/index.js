0f6217d4d7d1c32f09c254c4e10fdb7706056ff3
JohnyKovalenko1337 <13371488zeus@gmail.com>
2021-11-01 17:21:44 +0200

    added new service

--- service_3/index.js
+++ service_3/index.js
@@ -2,9 +2,9 @@ const express = require('express');
 
 const app = express();
 
-app.get('/api/service1 ', (req, res) => {
+app.get('/api/service3 ', (req, res) => {
   console.log('service 1 is working');
-  return res.json({message: 'hello'});
+  return res.json({message: 'hello from service 2'});
 });
 
 app.listen(8080, () => {
