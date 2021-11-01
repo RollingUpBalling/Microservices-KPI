e8e92fbc3ccbae1541d62f571c4aa1422ed8453e
17etro <petrpetryk2016@gmail.com>
2021-11-01 17:49:28 +0300

    complete request calls in frontend direction

--- service_2_client/src/App.js
+++ service_2_client/src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 import './App.css';
-import {useEffect} from "react";
 
 function App() {
+  const [firstString, setFirstString] = useState('loading...');
+  const [secondString, setSecondString] = useState('loading...');
+
+  useEffect(() => {
+    const firstRequest = axios.get('http://localhost:3001');
+    const secondRequest = axios.get('http://localhost:3002');
+
+    Promise.all([firstRequest, secondRequest])
+      .then((response) => {
+        // put into next two strings a response from each request replacing numbers
+        setFirstString('123');
+        setSecondString('321');
+      })
+      .catch((err) => {
+        setFirstString('error');
+        setSecondString('error');
+      });
+  }, []);
+
   return (
     <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+      <span>first string: {firstString}</span>
+      <span>second string: {secondString}</span>
     </div>
   );
 }
